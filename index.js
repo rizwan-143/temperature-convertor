a22b28c626b451a55fbc512e1fb28762e59f4aa4
rizwan-143 <164313822+rizwan-143@users.noreply.github.com>
2025-04-19 21:21:48 +0500

    new files

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 
-let dropdownBtn = document.getElementById("dropdownBtn");
- function selectUnit(unit){
-    dropdownBtn.textContent = unit;
- };
- let convert = document.getElementById("convert");
- convert.addEventListener(`click` , function(){
-     let userInput = document.getElementById("userInput").value;
-   if(userInput === "" ){
-    alert('enter a number !')
-   }else if(dropdownBtn.textContent === "fahrenheit" ){
-    let li = document.createElement("li");
-    let tempList = document.getElementById("tempList");
-    let result = (userInput * 9/5) + 32;
-    li.textContent = `The ${userInput} celsius is equal to ${result} fahrenheit `
-    tempList.appendChild(li);
-   }else{
-    let tempList = document.getElementById("tempList");
-    let li = document.createElement("li");
-     let result = (userInput - 32) * 5/9;
-    li.textContent = `The ${userInput} fahrenheit is equal to ${result} celsius `
-    tempList.appendChild(li);
-   }
- })
+  let dropdownBtn = document.getElementById("dropdownBtn");
+  function selectUnit(unit){
+      dropdownBtn.textContent = unit;
+  };
+  let convert = document.getElementById("convert");
+  convert.addEventListener(`click` , function(){
+      let userInput = document.getElementById("userInput").value;
+      let number = parseFloat(userInput);
+    if(userInput === "" || isNaN(number) ){
+      alert('enter a number !')
+    }else if(dropdownBtn.textContent === "fahrenheit" ){
+      let li = document.createElement("li");
+      let tempList = document.getElementById("tempList");
+      let result = ((number * 9/5) + 32).toFixed(2);
+      li.textContent = `The ${number} celsius is equal to ${result} fahrenheit `
+      tempList.appendChild(li);
+      userInput.value = "";
+    }else{
+      let tempList = document.getElementById("tempList");
+      let li = document.createElement("li");
+      let result = ((number - 32) * 5/9).toFixed(2);
+      li.textContent = `The ${number} fahrenheit is equal to ${result} celsius `
+      tempList.appendChild(li);
+      userInput.value = "";
+    }
+  })
 
 
