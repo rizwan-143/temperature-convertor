fe56dced16c927d589769585b157e24c9c2a1896
rizwan-143 <164313822+rizwan-143@users.noreply.github.com>
2025-04-19 21:40:52 +0500

    new files

--- index.js
+++ index.js
@@ -7,22 +7,23 @@
   convert.addEventListener(`click` , function(){
       let userInput = document.getElementById("userInput").value;
       let number = parseFloat(userInput);
+      
     if(userInput === "" || isNaN(number) ){
       alert('enter a number !')
+    }else if(dropdownBtn.textContent.trim() === "select Temp"){
+      alert("select a temperatur unit")
     }else if(dropdownBtn.textContent === "fahrenheit" ){
       let li = document.createElement("li");
       let tempList = document.getElementById("tempList");
       let result = ((number * 9/5) + 32).toFixed(2);
       li.textContent = `The ${number} celsius is equal to ${result} fahrenheit `
       tempList.appendChild(li);
-      userInput.value = "";
     }else{
       let tempList = document.getElementById("tempList");
       let li = document.createElement("li");
       let result = ((number - 32) * 5/9).toFixed(2);
       li.textContent = `The ${number} fahrenheit is equal to ${result} celsius `
       tempList.appendChild(li);
-      userInput.value = "";
     }
   })
 
